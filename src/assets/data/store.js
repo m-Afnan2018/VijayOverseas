3b16a3fbb805abe8efad8cf29459256a2d41f42b
m.Afnan <m.afnan2018@gmail.com>
2025-12-24 22:00:18 +0000

    Pause

---
+++ src/assets/data/store.js
@@ -0,0 +1,22 @@
+import item1 from '@/assets/images/home/item1'
+import item2 from '@/assets/images/home/item2'
+import item3 from '@/assets/images/home/item3'
+import item4 from '@/assets/images/home/item4'
+import item5 from '@/assets/images/home/item5'
+import item6 from '@/assets/images/home/item6'
+import item7 from '@/assets/images/home/item7'
+
+const image1 = item1;
+const image2 = item2;
+const image3 = item3;
+const image4 = item4;
+const image5 = item5;
+const image6 = item6;
+const image7 = item7;
+
+const storeData = [
+    {
+        image: image1,
+        
+    }
+]
---
+++ src/components/core/Home/Store/Store.jsx
@@ -0,0 +1,17 @@
+
+
+
+const storeData = [
+    {
+        image: 
+    }
+]
+
+export default function Store() {
+    return <section>
+        <h5>our menu</h5>
+        <h2>Discover a collection of MULTIPLE JAGGERY</h2>
+        <button>view all menu</button>
+
+    </section>
+}
